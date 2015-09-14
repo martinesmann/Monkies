7be06d1833265181d0f8e7b39855d1fc9f46a3e1
Martin Esmann <martin.esmann@hotmail.com>
2015-09-14 15:05:05 +0200

    Tutorial Part#2 - Sync

--- src/Monkies/DummyData.cs
+++ src/Monkies/DummyData.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using Couchbase.Lite;
 using Newtonsoft.Json;
+using System.Diagnostics;
 
 namespace Monkies
 {
@@ -16,16 +17,57 @@ namespace Monkies
 
         public static void Init()
         {
-            Manager.SharedInstance.GetDatabase("cb").Delete();
+            //Manager.SharedInstance.GetDatabase("cb").Delete();
             Manager.SharedInstance.GetDatabase("cb").Changed += DummyData_Changed; ;
-           
-            var monkey = new Monkey
-            {
-                Name = "Orangutan",
-                Description = "Native to Indonesia and Malaysia, orangutans are currently found in only the rainforests of Borneo and Sumatra. "
-            };
 
-            Add(monkey);
+            var exsisting =
+                Manager.SharedInstance.GetDatabase("cb")
+                .CreateAllDocumentsQuery()
+                .Run()
+                // added check to filter out all none valid documents
+                .Where(doc => doc.Document != null)
+                .Where(doc => doc.Document.UserProperties != null)
+                .Where(doc => doc.Document.UserProperties.ContainsKey("doc"))
+                .Where(doc => doc.Document.UserProperties.ContainsKey("type"))
+                .Where(doc => doc.Document.UserProperties["type"].ToString() == "monkey")
+                .ToList();
+
+            exsisting
+                .ForEach(doc => Monkies.Add(JsonConvert.DeserializeObject<Monkey>(doc.Document.UserProperties["doc"].ToString())));
+
+            // only add default data if database is empty
+            if (!exsisting.Any())
+            {
+                var monkey = new Monkey
+                {
+                    Name = "Orangutan",
+                    Description = "Native to Indonesia and Malaysia, orangutans are currently found in only the rainforests of Borneo and Sumatra. "
+                };
+
+                Add(monkey);
+            }
+
+            InitSync();
+        }
+
+        private static void InitSync()
+        {
+            // this is all the code needed to initialize and continue sync'ing with SyncGateway.
+            string syncGAtewayUrl = "http://cbdemo004bizz.cloudapp.net:4984/sync_gateway/";
+            string user = "couchbase";
+            string pass = "letmein";
+
+            // Pull any changes from Sync Gateway to the local database as changes happen
+            var pull = Manager.SharedInstance.GetDatabase("cb").CreatePullReplication(new Uri(syncGAtewayUrl));
+            pull.Authenticator = new Couchbase.Lite.Auth.BasicAuthenticator(user, pass);
+            pull.Continuous = true;
+            pull.Start();
+
+            // Pull any local changes to SyncGateway as changes happen
+            var push = Manager.SharedInstance.GetDatabase("cb").CreatePushReplication(new Uri(syncGAtewayUrl));
+            push.Authenticator = new Couchbase.Lite.Auth.BasicAuthenticator(user, pass);
+            push.Continuous = true;
+            push.Start();
         }
 
         private static void DummyData_Changed(object sender, DatabaseChangeEventArgs e)
@@ -34,6 +76,7 @@ namespace Monkies
 
             if (e.IsExternal)
             {
+                Debugger.Break();
                 // this change came from external sync
             }
 
@@ -41,7 +84,12 @@ namespace Monkies
                 e.Changes
                 .ToList()
                 .Select(item => item.DocumentId)
-                .Select(id => db.GetDocument(id));
+                .Select(id => db.GetDocument(id))
+                // added check to filter out all none valid documents
+                .Where(doc => doc.Properties != null)
+                .Where(doc => doc.Properties.ContainsKey("doc"))
+                .Where(doc => doc.Properties.ContainsKey("type"))
+                .Where(doc => doc.Properties["type"].ToString() == "monkey");
 
             foreach (var document in documents)
             {
